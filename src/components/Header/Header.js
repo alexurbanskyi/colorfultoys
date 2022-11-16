b459d41a4766d479f5b8da778149c950e4880f2b
kalitsinskyi <vlad.kalitsinskij@transfotech.com>
2022-11-16 21:59:24 +0200

    logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Header.css'
-import logo from '../../images/logo.png'
+import logo from '../../images/LogoNew.png'
 import back from '../../images/back.png'
 
 function Header() {
