ae613ce2f3c73250cada7a903ce3ab0322bb9ed8
kalitsinskyi <vlad.kalitsinskij@transfotech.com>
2022-11-16 19:26:52 +0200

    logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Header.css'
 import logo from '../../images/logo.png'
+import back from '../../images/back.png'
 
 function Header() {
   return (
-    <div className='header'>
+    <div className='header' style={{ backgroundImage: `url(${back})` }}>
       <img src={logo}/>
     </div>
   )
