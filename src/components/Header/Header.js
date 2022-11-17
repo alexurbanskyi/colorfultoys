cd6ca6664fe349a519a48f4e20f25f1cccdcb09c
kalitsinskyi <vlad.kalitsinskij@transfotech.com>
2022-11-18 00:13:53 +0200

    add basket

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,15 @@ import React from 'react'
 import './Header.css'
 import logo from '../../images/LogoNew.png'
 import back from '../../images/back.png'
+import { SlBasket } from "react-icons/sl";
 
 function Header() {
   return (
     <div className='header' style={{ backgroundImage: `url(${back})` }}>
       <img src={logo}/>
+      <div className='basket' onClick={() => console.log('BUY!!')}>
+        <SlBasket size={'35px'}/>
+      </div>
     </div>
   )
 }
